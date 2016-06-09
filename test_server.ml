f04809d4b0ff131a781f88fb3bcd4244c251dbaf
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-06-08 20:05:44 -0700

    Found Hidden DOM_Server

--- test_server.ml
+++ test_server.ml
@@ -11,23 +11,23 @@ let () =
 
   let reactjs_code = {| (function(){
 
- var ExampleApplication = React.createClass({
-        render: function() {
-          var elapsed = Math.round(this.props.elapsed  / 100);
-          var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-          var message =
-            'React has been successfully running for ' + seconds + ' seconds.';
-
-          return React.DOM.p(null, message);
+var Counter = React.createClass({
+        getInitialState: function () {
+          return { clickCount: 0 };
+        },
+        handleClick: function () {
+          this.setState(function(state) {
+            return {clickCount: state.clickCount + 1};
+          });
+        },
+        render: function () {
+    return React.DOM.h2({onClick:this.handleClick},
+                        "Click Me! Number of clicks:" + this.state.clickCount);
         }
       });
-      var ExampleApplicationFactory = React.createFactory(ExampleApplication);
 
-      var start = new Date().getTime();
-    var e = ExampleApplicationFactory({elapsed: new Date().getTime() - start});
-
-    return React.__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.
-renderToString(React.createElement(ExampleApplication), null);
+   return React.__SECRET_DOM_SERVER_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.
+                        renderToString(React.createElement(Counter), null);
 
 })();
 |}
@@ -35,7 +35,6 @@ renderToString(React.createElement(ExampleApplication), null);
 
   let callback conn _ _ =
     let react_created_body = exec_js ctx reactjs_code in
-    print_endline react_created_body;
     S.respond_string ~status:`OK ~body:react_created_body ()
   in
   let server = S.make callback () in
