ededb1848ad4695da06889bc90cbcd7cbe9a0821
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-06-11 02:25:51 -0700

    Drop down to C API since built on Linux

--- jscore_stubs.cpp
+++ jscore_stubs.cpp
@@ -10,25 +10,39 @@
 #include <iostream>
 #include <string>
 
-#include <Foundation/Foundation.h>
-#include <JavaScriptCore/JavaScriptCore.h>
+#include <JavaScriptCore/JavaScript.h>
+
+// Maybe good usage of C++ for abstracting this hassle.
+// class JSVM {
+
+// };
+
 
 extern "C" {
 
   CAMLprim value exec_js(value ctx, value js_string)
   {
     const char *const result = caml_strdup(String_val(js_string));
-    JSContext *_context = (JSContext*)ctx;
-    JSValue *evaled =
-      [_context evaluateScript:
-		  [NSString stringWithUTF8String:result]];
-    return caml_copy_string([[evaled toString] UTF8String]);
+
+    JSGlobalContextRef context = (JSGlobalContextRef)ctx;
+
+    JSStringRef code = JSStringCreateWithUTF8CString(result);
+
+    JSObjectRef globalObject = JSContextGetGlobalObject(context);
+    JSValueRef exception;
+    JSValueRef js_result = JSEvaluateScript(context, code, globalObject, NULL, 1, &exception);
+
+    JSStringRef result_as_string = JSValueToStringCopy(context, js_result, &exception);
+    size_t string_len = JSStringGetMaximumUTF8CStringSize(result_as_string);
+    char *string_buffer = (char*)malloc(string_len);
+    JSStringGetUTF8CString(result_as_string, string_buffer, string_len);
+    JSStringRelease(code);
+    return caml_copy_string(string_buffer);
   }
 
   CAMLprim value create_js_context_ml(__unused value unit)
   {
-    return (value)[[JSContext alloc]
-		    initWithVirtualMachine:[[JSVirtualMachine alloc] init]];
+    return (value)JSGlobalContextCreateInGroup(NULL, NULL);
   }
 
 }
