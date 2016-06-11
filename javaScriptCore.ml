848afce1baf129923ba181f65c0ea6b1e220cdc0
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-06-11 16:09:49 -0700

    Sketch out the submodules

--- javaScriptCore.ml
+++ javaScriptCore.ml
@@ -7,3 +7,19 @@ external evaluate_script : ctx:context -> string -> string = "eval_js_ml"
 external check_script_syntax : context -> string -> bool = "check_syntax_js_ml"
 
 external garbage_collect : context -> unit = "gc_js_ml"
+
+module Object = struct
+
+end
+
+module String = struct
+
+end
+
+module Value = struct
+
+end
+
+module Context = struct
+
+end
